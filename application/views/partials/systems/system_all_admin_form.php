89d2c024c737e416207b3bd5ba6416068f3b95e3
wzhjay <wzhjay@gmail.com>
2014-05-16 04:09:00 +0800

    add modal event for admin edit and delete, add keypress event for admin search

--- application/views/partials/systems/system_all_admin_form.php
+++ application/views/partials/systems/system_all_admin_form.php
@@ -6,6 +6,8 @@
 			event.preventDefault();
 			
 			load_admin_users();
+
+
 		});
 
 		function load_admin_users() {
@@ -20,7 +22,97 @@
 			    		var reply = $.parseJSON(json);
 			    		for (var key in reply) {
 			    			if (reply.hasOwnProperty(key)) {
-			    				admins.append('<tr id="user_id_'+ reply[key].user_id +'">' + '<th>' + reply[key].username + '</th>' + '<th>' + reply[key].email + '</th>' + '<th>' + reply[key].name + '</th>' + '<th>' + reply[key].role + '</th>' + '<th>' + reply[key].status + '</th>' + '</tr>');
+			    				admins.append('<tr id="user_id_'+ reply[key].user_id +'">' + '<th>' + reply[key].username + '</th>' + '<th>' + reply[key].email + '</th>' + '<th>' + reply[key].name + '</th>' + '<th>' + reply[key].role + '</th>' + '<th>' + reply[key].status + '</th>' + '<th><a class="button glow button-rounded button-flat admin-edit" id="admin_edit_' + reply[key].user_id + '" data-toggle="modal" data-target="#admin-edit-model">Edit</a></th>' + '<th><a class="button glow button-rounded button-flat admin-delete" id="admin_delete_' + reply[key].user_id + '" data-toggle="modal" data-target="#admin-del-model">Del</a></th>' + '</tr>');
+			    			}
+			    		}
+
+			    		$.each($('.admin-edit'), function(i, v) {
+							$(v).on('click', function() {
+								var unsername = $(this).closest('tr').find('th').first().text();
+								var el_id = $(this).attr('id').split('_');
+								var user_id = el_id[2];
+								// alert(user_id);
+								var modalBody = $('#adminEdit').closest('.modal-content').find('.modal-body');
+								modalBody.children().remove();
+								modalBody.append(
+									'<div class="row">' + 
+										'<div class="col-xs-4">'+ 
+											'<label>用户名</label>' +
+											'<div>' + unsername + '</div>' +
+										'</div>' + 
+										'<div class="col-xs-4">' + 
+											'<label for="input_system_assigned_role_edit">权限分配</label>' + 
+											'<select class="form-control" id="input_system_assigned_role_edit"></select>' + 
+										'</div>' +
+										'<div class="col-xs-4">' +
+											'<label for="input_system_assigned_branch_edit">分部</label>' +
+											'<select class="form-control" id="input_system_assigned_branch_edit"></select>' + 
+										'</div>' +
+									'</div>');
+								load_roles_edit();
+								load_branches_edit();
+							});
+						});
+
+						$.each($('.admin-delete'), function(i, v) {
+							$(v).on('click', function() {
+								var unsername = $(this).closest('tr').find('th').first().text();
+								var el_id = $(this).attr('id').split('_');
+								var user_id = el_id[2];
+								// alert(user_id);
+								var modalBody = $('#adminDel').closest('.modal-content').find('.modal-body');
+								modalBody.children().remove();
+								modalBody.append(
+									'<div class="row">' + 
+										'<div class="col-xs-4">'+
+											'<label>用户名</label>' +
+											'<div>' + unsername + '</div>' +
+										'</div>' + 
+									'</div>');
+							});
+						});
+			        }else{
+			        	alert("fail to load braches");
+			        }
+			    },
+			});//End ajax
+		}
+
+		function load_roles_edit() {
+			var roles = $('#input_system_assigned_role_edit');
+			$.ajax({
+				type:"post",
+			    url:window.api_url + "getAllAdminRoles",
+			    data:{},
+			    success:function(json){
+			    	roles.children().remove();
+			    	if(json != null) {
+			    		var reply = $.parseJSON(json);
+			    		for (var key in reply) {
+			    			if (reply.hasOwnProperty(key)) {
+			    				roles.append('<option id="admin_role_'+ reply[key].id +'_edit">' + reply[key].role + '</option>');
+			    			}
+			    		}
+			        }else{
+			        	alert("fail to load roles");
+			        }
+			    },
+			});//End ajax
+		}
+
+		function load_branches_edit() {
+			var branches = $('#input_system_assigned_branch_edit');
+			$.ajax({
+				type:"post",
+			    url:window.api_url + "getAllBranches",
+			    data:{},
+			    success:function(json){
+			    	branches.children().remove();
+			    	if(json != null) {
+			    		var reply = $.parseJSON(json);
+			    		for (var key in reply) {
+			    			if (reply.hasOwnProperty(key)) {
+			    				branches.append('<option id="admin_branch_'+ reply[key].id +'_edit">' + reply[key].name + '</option>');
 			    			}
 			    		}
 			        }else{
@@ -46,4 +138,40 @@
 	      <tbody id="system_all_admin_table_body"></tbody>
 	    </table>
 	</div>
+</div>
+
+<!-- Edit Modal -->
+<div class="modal fade" id="admin-edit-model" tabindex="-1" role="dialog" aria-labelledby="adminEdit" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="adminEdit">Edit</h4>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Del Modal -->
+<div class="modal fade" id="admin-del-model" tabindex="-1" role="dialog" aria-labelledby="adminDel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="adminDel">Delete this admin user?</h4>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Confirm</button>
+      </div>
+    </div>
+  </div>
 </div>
--- application/views/partials/systems/system_all_search_form.php
+++ application/views/partials/systems/system_all_search_form.php
@@ -4,55 +4,71 @@
 	<script>
 		$(document).ready(function($) {
 			event.preventDefault();
-			var target = $('#system_admin_search_results');
+
+			// search button click event
 			$('#system_admin_search_submit').on('click', function() {
-				var key_word = $('#input_system_admin_search_username').val();
-				target.children().remove();
-				target.append('<div class="loading"></div>');
-				$.ajax({
-			    	type:"post",
-			        url:window.api_url + "searchAdminUser",
-			        data:{key_word:key_word},
-			        success:function(json){
-			        	target.children().remove();
-			        	target.append('<p>No user found</p>');
-			            if(json != null) {
-			            	var reply = $.parseJSON(json);
-			            	if(reply.length > 0 ) {
-			            		target.children().remove();
-				            	target.append('<table class="table table-striped"><thead><tr><th>Username</th><th>Email</th><th>branch</th><th>Role</th><th>Status</th></tr></thead><tbody id="system_search_admin_table_body"></tbody></table>');
-				            	var table_body = $('#system_search_admin_table_body');
-				            	for (var key in reply) {
-					    			if (reply.hasOwnProperty(key)) {
-					            		table_body.append('<tr id="user_id_'+ reply[key].user_id +'">' + '<th>' + reply[key].username + '</th>' + '<th>' + reply[key].email + '</th>' + '<th>' + reply[key].name + '</th>' + '<th>' + reply[key].role + '</th>' + '<th>' + reply[key].status + '</th>' + '</tr>');
-					            	}
-					            }
-				        	}
-			            }else {
-			            	alert("fail to call search api");
-			            }
-			        }
-			    });//End ajax
+				search_admin_user();
 			});	// end of click event
+			
+
+			// enter key press event, when focus on inupt
+			$(document).keypress(function(event){
+				if ($("#input_system_admin_search_username").is(":focus")) {
+				  	var keycode = (event.keyCode ? event.keyCode : event.which);
+			        if(keycode == '13'){
+			            search_admin_user();
+			        }
+			        event.stopPropagation();
+				}
+		    });
 		});
+
+
+		function search_admin_user() {
+			var target = $('#system_admin_search_results');
+			var key_word = $('#input_system_admin_search_username').val();
+			target.children().remove();
+			target.append('<div class="loading"></div>');
+			$.ajax({
+				type:"post",
+			    url:window.api_url + "searchAdminUser",
+			    data:{key_word:key_word},
+			    success:function(json){
+			    	target.children().remove();
+			        target.append('<p>No user found</p>');
+			        if(json != null) {
+			        	var reply = $.parseJSON(json);
+			            if(reply.length > 0 ) {
+			            	target.children().remove();
+				            target.append('<table class="table table-striped"><thead><tr><th>Username</th><th>Email</th><th>branch</th><th>Role</th><th>Status</th></tr></thead><tbody id="system_search_admin_table_body"></tbody></table>');
+				            var table_body = $('#system_search_admin_table_body');
+				            for (var key in reply) {
+					    		if (reply.hasOwnProperty(key)) {
+					            	table_body.append('<tr id="user_id_'+ reply[key].user_id +'">' + '<th>' + reply[key].username + '</th>' + '<th>' + reply[key].email + '</th>' + '<th>' + reply[key].name + '</th>' + '<th>' + reply[key].role + '</th>' + '<th>' + reply[key].status + '</th>' + '</tr>');
+					            }
+					        }
+						}
+			        }else {
+			        	alert("fail to call search api");
+					}
+			    }
+			});//End ajax
+		}
 	</script>
 </head>
 <div class="highlight">
 	<div class="row">
-		<form role="form">
-			<div id="system_admin_search">
-				<div class="col-xs-4">
-					<label for="input_system_admin_search_username">请输入管理员用户名/邮件关键字：</label>
-				</div>
-				<div class="col-xs-4">
-					<input class="form-control" id="input_system_admin_search_username" placeholder="keyworkd in username/email">
-				</div>
+		<div id="system_admin_search">
+			<div class="col-xs-4">
+				<label for="input_system_admin_search_username">请输入管理员用户名/邮件关键字：</label>
 			</div>
-		</form>
-		<div class="col-xs-2">
-		</div>
-		<div class="col-xs-2">
-			<a class="button glow button-rounded button-flat" id="system_admin_search_submit">Search</a>
+			<div class="col-xs-4">
+				<input class="form-control" id="input_system_admin_search_username" placeholder="keyworkd in username/email">
+			</div>
+			<div class="col-xs-2"></div>
+			<div class="col-xs-2">
+				<a class="button glow button-rounded button-flat" id="system_admin_search_submit">Search</a>
+			</div>
 		</div>
 	</div>
 	<div id="system_admin_search_results"></div>
