53eb5368291179d36cd8446bdbde77b1148c3aba
wzhjay <wzhjay@gmail.com>
2014-05-23 22:06:34 +0800

    modify student searched data

--- application/views/partials/students/student_search_form.php
+++ application/views/partials/students/student_search_form.php
@@ -19,6 +19,194 @@
 		    });
 		});
 
+		function search_get_student_record_by_ic(key_word, target) {
+			$.ajax({
+				type:"post",
+				url:window.api_url + "getStudentRecordsByIC",
+				data:{ic:key_word},
+				success:function(json){
+					if(json != null) {
+						var reply = $.parseJSON(json);
+						if(reply.length > 0 ) {
+							target.append('<h4>Sutdent Exam Records (考试记录)</h4><hr>');
+							for (var key in reply) {
+								var num = parseInt(key) + 1;
+								if (reply.hasOwnProperty(key)) {
+									target.append(
+										'<div class="panel-group" id="exam_record_collapse_'+key+'">' +
+											'<div class="panel panel-default">' + 
+												'<div class="panel-heading">' +
+													'<h4 class="panel-title">' +
+														'<a data-toggle="collapse" data-parent="exam_record_collapse_'+key+'" href="#exam_record_collapse_body_'+key+'">Exam Record ' + num + '</a>' + 
+													' </h4>' +
+												'</div>' +
+												'<div id="exam_record_collapse_body_'+key+'" class="panel-collapse collapse">' + 
+												'<div class="panel-body">' + 
+													'<div class="row">' + 
+														'<div class="col-xs-3">'+ 
+															'<div>Exam Date: ' + reply[key].exam_date + '</div>' +
+														'</div>' + 
+													'</div>' +
+													'<hr>' +  
+													'<div class="row">' + 
+														'<div class="col-xs-2">'+ 
+															'<div>EL Best: ' + reply[key].el_best + '</div>' +
+														'</div>' + 
+														'<div class="col-xs-2">' +
+															'<div>ER Best: ' + reply[key].er_best + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-2">' +
+															'<div>ES Best: '+ reply[key].es_best + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-2">' +
+															'<div>EW Best: '+ reply[key].ew_best + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-2">' +
+															'<div>EN Best: '+ reply[key].en_best + '</div>' + 
+														'</div>' +
+													'</div>' + 
+													'<hr>' + 
+													'<div class="row">' + 
+														'<div class="col-xs-3">'+ 
+															'<div>CMP: ' + reply[key].cmp + '</div>' +
+														'</div>' + 
+														'<div class="col-xs-3">' +
+															'<div>CON: ' + reply[key].con +'</div>' + 
+														'</div>' +
+														'<div class="col-xs-3">' +
+															'<div>WRI: '+ reply[key].wri + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-3">' +
+															'<div>WPN: '+ reply[key].wpn + '</div>' + 
+														'</div>' +
+													'</div>' + 
+													'<div class="row">' + 
+														'<div class="col-xs-3">'+ 
+															'<div>Created: ' + reply[key].created + '</div>' +
+														'</div>' + 
+														'<div class="col-xs-3">' +
+															'<div>Modified: ' + reply[key].modified + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-3">' +
+															'<div>Remark: '+ reply[key].remark + '</div>' + 
+														'</div>' +
+													'</div>' + 
+												'</div>' + 
+											'</div>' +
+										'</div>'
+									);
+								}
+							}
+							target.append('<br>');
+						} else {
+							target.append('<p>No Exam Records found</p><br>');
+						}
+					}else {
+						alert("fail to call search api");
+					}
+				}
+			});//End ajax
+		}
+
+		function search_get_student_ato_by_ic(key_word, target) {
+			$.ajax({
+				type:"post",
+				url:window.api_url + "getStudentATOInfoByIC",
+				data:{ic:key_word},
+				success:function(json){
+					if(json != null) {
+						var reply = $.parseJSON(json);
+						if(reply.length > 0 ) {
+							target.append('<h4>Sutdent ATO Information（ATO信息）</h4><hr>');
+							for (var key in reply) {
+								var num = parseInt(key) + 1;
+								if (reply.hasOwnProperty(key)) {
+									target.append(
+										'<div class="panel-group" id="ato_collapse_'+key+'">' +
+											'<div class="panel panel-default">' + 
+												'<div class="panel-heading">' +
+													'<h4 class="panel-title">' +
+														'<a data-toggle="collapse" data-parent="ato_collapse_'+key+'" href="#ato_collapse_body_'+key+'">ATO ' + num + '</a>' + 
+													' </h4>' +
+												'</div>' +
+												'<div id="ato_collapse_body_'+key+'" class="panel-collapse collapse">' + 
+												'<div class="panel-body">' + 
+													'<div class="row">' + 
+														'<div class="col-xs-3">'+ 
+															'<div>Exam Type: ' + reply[key].pre_post + '</div>' +
+														'</div>' + 
+														'<div class="col-xs-3">' +
+															'<div>Class Start Date: ' + reply[key].class_start_date + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-3">' +
+															'<div>Class End Date: '+ reply[key].class_end_date + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-3">' +
+															'<div>Class Code: '+ reply[key].class_code + '</div>' + 
+														'</div>' +
+													'</div>' + 
+													'<div class="row">' + 
+														'<div class="col-xs-3">'+ 
+															'<div>Attendance: ' + reply[key].attendance + '</div>' +
+														'</div>' + 
+														'<div class="col-xs-3">' +
+															'<div>Recommend Level: ' + reply[key].recommend_level + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-3">' +
+															'<div>Exam Date&Time: '+ reply[key].exam_date + ' ' + reply[key].exam_time + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-3">' +
+															'<div>Exam Location: '+ reply[key].exam_location + '</div>' + 
+														'</div>' +
+													'</div>' + 
+													'<hr>' + 
+													'<div class="row">' + 
+														'<div class="col-xs-2">'+ 
+															'<div>EL: ' + reply[key].el + '</div>' +
+														'</div>' + 
+														'<div class="col-xs-2">' +
+															'<div>ER: ' + reply[key].er +'</div>' + 
+														'</div>' +
+														'<div class="col-xs-2">' +
+															'<div>EN: '+ reply[key].en + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-2">' +
+															'<div>ES: '+ reply[key].es + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-2">' +
+															'<div>EW: '+ reply[key].ew + '</div>' + 
+														'</div>' +
+													'</div>' + 
+													'<div class="row">' + 
+														'<div class="col-xs-3">'+ 
+															'<div>Created: ' + reply[key].created + '</div>' +
+														'</div>' + 
+														'<div class="col-xs-3">' +
+															'<div>Modified: ' + reply[key].modified + '</div>' + 
+														'</div>' +
+														'<div class="col-xs-3">' +
+															'<div>Remark: '+ reply[key].remark + '</div>' + 
+														'</div>' +
+													'</div>' + 
+												'</div>' + 
+											'</div>' +
+										'</div>'
+									);
+								}
+							}
+							target.append('<br>');
+
+							search_get_student_record_by_ic(key_word, target);
+						} else {
+							target.append('<p>No ATO Information found</p><br>');
+						}
+					}else {
+						alert("fail to call search api");
+					}
+				}
+			});//End ajax
+		}
+
 		function search_student_info() {
 			var key_word = $('#input_student_search_ic').val();
 			var target = $('#student_search_results');
@@ -36,67 +224,135 @@
 			            if(reply.length > 0 ) {
 			            	target.empty();
 			            	target.append(
-					            '<h4>Sutdent Basic Information</h4><hr>');
+					            '<h4>Sutdent Basic Information(基本信息，工作，联系方式)</h4><hr>');
 				            for (var key in reply) {
 					    		if (reply.hasOwnProperty(key)) {
 					            	target.append(
-					            		JSON.stringify(reply[key]) + 
-					            		'<br>');
+					            		'<div class="row">' + 
+											'<div class="col-xs-3">'+ 
+												'<div>IC Number: ' + reply[key].ic + '</div>' +
+											'</div>' + 
+											'<div class="col-xs-3">' +
+												'<div>Name: ' + reply[key].salutation + ' ' + reply[key].firstname + ' ' + reply[key].lastname + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Register Date: '+ reply[key].reg_date + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Register Number: '+ reply[key].reg_no + '</div>' + 
+											'</div>' +
+										'</div>' + 
+										'<div class="row">' + 
+											'<div class="col-xs-3">'+ 
+												'<div>IC Type: ' + reply[key].ic_type + '</div>' +
+											'</div>' + 
+											'<div class="col-xs-3">' +
+												'<div>Nationality: ' + reply[key].nationality + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Gender: '+ reply[key].gender + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Birthday: '+ reply[key].birthday + '</div>' + 
+											'</div>' +
+										'</div>' + 
+										'<div class="row">' + 
+											'<div class="col-xs-3">'+ 
+												'<div>Age: ' + reply[key].age + '</div>' +
+											'</div>' + 
+											'<div class="col-xs-3">' +
+												'<div>Citizenship: ' + reply[key].citizenship + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Race: '+ reply[key].race + '</div>' + 
+											'</div>' +
+										'</div>' + 
+										'<div class="row">' + 
+											'<div class="col-xs-3">' +
+												'<div>Language: '+ reply[key].lang + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Chinese Level: ' + reply[key].cn_level + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Edu level: '+ reply[key].edu_level + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Start Date Wanted: '+ reply[key].start_date_wanted + '</div>' + 
+											'</div>' +
+										'</div>' + 
+										'<hr>' +
+										'<div class="row">' + 
+											'<div class="col-xs-3">'+ 
+												'<div>Source: ' + reply[key].source + '</div>' +
+											'</div>' + 
+											'<div class="col-xs-3">' +
+												'<div>Created: ' + reply[key].created + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Modified: '+ reply[key].modified + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">'+ 
+												'<div>Government Letter: ' + reply[key].gov_letter + '</div>' +
+											'</div>' + 
+										'</div>' + 
+										'<div class="row">' + 
+											'<div class="col-xs-3">'+ 
+												'<div>Employment Status: ' + reply[key].emp_status + '</div>' +
+											'</div>' + 
+											'<div class="col-xs-3">' +
+												'<div>Company Name: ' + reply[key].company_name + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Company Type: '+ reply[key].company_type + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">'+ 
+												'<div>Company Reg No.: ' + reply[key].company_reg_no + '</div>' +
+											'</div>' + 
+										'</div>' + 
+										'<div class="row">' + 
+											'<div class="col-xs-3">'+ 
+												'<div>Industry: ' + reply[key].industry + '</div>' +
+											'</div>' + 
+											'<div class="col-xs-3">' +
+												'<div>Designation: ' + reply[key].designation + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Salary Range: '+ reply[key].salary_range + '</div>' + 
+											'</div>' +
+										'</div>' + 
+										'<hr>' + 
+										'<div class="row">' + 
+											'<div class="col-xs-3">'+ 
+												'<div>Tel: ' + reply[key].tel + '</div>' +
+											'</div>' + 
+											'<div class="col-xs-3">' +
+												'<div>Home Tel: ' + reply[key].tel_home + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Street: '+ reply[key].street + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Block: '+ reply[key].block + '</div>' + 
+											'</div>' +
+										'</div>' + 
+										'<div class="row">' + 
+											'<div class="col-xs-3">'+ 
+												'<div>#Floor-Unit: ' + reply[key].floor_unit_no + '</div>' +
+											'</div>' + 
+											'<div class="col-xs-3">' +
+												'<div>Building Name: ' + reply[key].building + '</div>' + 
+											'</div>' +
+											'<div class="col-xs-3">' +
+												'<div>Postcode: '+ reply[key].postcode + '</div>' + 
+											'</div>' +
+										'</div>' +
+					            		'<hr>');
 					            }
 					        }
 					        target.append('<br>');
 
-					        $.ajax({
-								type:"post",
-							    url:window.api_url + "getStudentATOInfoByIC",
-							    data:{ic:key_word},
-							    success:function(json){
-							        if(json != null) {
-							        	var reply = $.parseJSON(json);
-							            if(reply.length > 0 ) {
-							            	target.append('<h4>Sutdent ATO Information</h4><hr>');
-								            for (var key in reply) {
-									    		if (reply.hasOwnProperty(key)) {
-									            	target.append(
-									            		JSON.stringify(reply[key]) + 
-									            		'<br>');
-									            }
-									        }
-									        target.append('<br>');
-
-									        $.ajax({
-												type:"post",
-											    url:window.api_url + "getStudentRecordsByIC",
-											    data:{ic:key_word},
-											    success:function(json){
-											        if(json != null) {
-											        	var reply = $.parseJSON(json);
-											            if(reply.length > 0 ) {
-											            	target.append('<h4>Sutdent Exam Records</h4><hr>');
-												            for (var key in reply) {
-													    		if (reply.hasOwnProperty(key)) {
-													            	target.append(
-													            		JSON.stringify(reply[key]) + 
-													            		'<br>');
-													            }
-													        }
-													        target.append('<br>');
-														} else {
-															target.append('<p>No Exam Records found</p><br>');
-														}
-											        }else {
-											        	alert("fail to call search api");
-													}
-											    }
-											});//End ajax
-										} else {
-											target.append('<p>No ATO Information found</p><br>');
-										}
-							        }else {
-							        	alert("fail to call search api");
-									}
-							    }
-							});//End ajax
+					        search_get_student_ato_by_ic(key_word, target);
 						}
 			        }else {
 			        	alert("fail to call search api");
