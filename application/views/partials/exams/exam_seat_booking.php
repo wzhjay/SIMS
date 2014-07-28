a20c619fefd6155ee8c0f0f4c47e83e0188a7ce0
wzhjay <wzhjay@gmail.com>
2014-07-28 18:56:31 +0800

    calender design

--- application/views/partials/exams/exam_seat_booking.php
+++ application/views/partials/exams/exam_seat_booking.php
@@ -7,12 +7,108 @@
 
 	</script>
 </head>
-<div class="highlight">
+<div class="highlight" id="exam_seat_booking_calendar_section_highlight">
 	<div class="row">
-		<div class="col-xs-2" id="exam_seat_booking_event_section">
-		</div>
-		<div class="col-xs-10" id="exam_seat_booking_calendar_section">
-			<div id='exam_seat_booking_calendar'></div>
+		<div class="col-xs-12" id="exam_seat_booking_calendar_section">
+			<div id='exam_seat_booking_calendar'>
+				<div class="row">
+					<div class="col-xs-5"></div>
+					<div class="col-xs-4"><h1><span class="label label-info">输入座位数目</span></h1></div>
+					<div class="col-xs-3"></div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<ul class="pager">
+						  <li class="previous"><a href="#">&larr; Prebious</a></li>
+						  <li class="next"><a href="#">Next &rarr;</a></li>
+						</ul>
+					</div>
+				</div>
+				<table class="table table-bordered">
+  					<thead>
+				        <tr>
+				          <th>Sun</th>
+				          <th>Mon</th>
+				          <th>Tue</th>
+				          <th>Wed</th>
+				          <th>Thu</th>
+				          <th>Fri</th>
+				          <th>Sat</th>
+				        </tr>
+				    </thead>
+				    <tbody>
+				        <tr>
+				          <td>
+				          	<div class="exam_seat_booking_content_each_date">
+					          	<div id='exam_seat_booking_date_1'>1</div>
+					          	<table class="table table-bordered">
+									<tbody>
+										<tr>
+											<td></td>
+								          	<td>JE</td>
+								          	<td>UN</td>
+										</tr>
+										<tr>
+											<td>09:00</td>
+								          	<td><input type="text" class="form-control" value="12"></td>
+								          	<td><input type="text" class="form-control" value="0"></td>
+										</tr>
+										<tr>
+											<td>14:00</td>
+								          	<td><input type="text" class="form-control" value="12"></td>
+								          	<td><input type="text" class="form-control" value="0"></td>
+										</tr>
+										<tr>
+											<td>19:00</td>
+								          	<td><input type="text" class="form-control" value="12"></td>
+								          	<td><input type="text" class="form-control" value="0"></td>
+										</tr>
+									</tbody>	
+								</table>
+							</div>
+				          </td>
+				          <td>2</td>
+				          <td>3</td>
+				          <td>4</td>
+				          <td>5</td>
+				          <td>6</td>
+				          <td>7</td>
+				        </tr>
+				        <tr>
+				          <td>8</td>
+				          <td>9</td>
+				          <td>10</td>
+				          <td>11</td>
+				          <td>12</td>
+				          <td>13</td>
+				          <td>14</td>
+				        </tr>
+				        <tr>
+				          <td>15</td>
+				          <td>16</td>
+				          <td>17</td>
+				          <td>18</td>
+				          <td>19</td>
+				          <td>20</td>
+				          <td>21</td>
+				        </tr>
+				        <tr>
+				          <td>22</td>
+				          <td>23</td>
+				          <td>24</td>
+				          <td>25</td>
+				          <td>26</td>
+				          <td>27</td>
+				          <td>28</td>
+				        </tr>
+				        <tr>
+				          <td>29</td>
+				          <td>30</td>
+				          <td>31</td>
+				        </tr>
+				      </tbody>
+				</table>
+			</div>
 		</div>
 	</div>
 	<div class="row">
