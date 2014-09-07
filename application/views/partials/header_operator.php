3953c3d82232f0304d08b2a0f0835ddeaf5ccef8
wzhjay <wzhjay@gmail.com>
2014-09-07 18:21:21 +0800

    fix bugs in date header

--- application/views/partials/header_operator.php
+++ application/views/partials/header_operator.php
@@ -3,7 +3,7 @@
 <script>
 var fullDate = new Date()
 var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
-var twoDigitDate = ((fullDate.getDate().length+1) === 1)? (fullDate.getDate()+1) : '0' + (fullDate.getDate()+1);
+var twoDigitDate = ((fullDate.getDate().length+1) === 1)? (fullDate.getDate()) : '0' + (fullDate.getDate());
 var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
 
 $(document).ready(function() {
